0dd918cee2b5c9aa17ccc0bed373e184c1609a64
JanFromTheBlock <133579648+JanFromTheBlock@users.noreply.github.com>
2024-01-11 23:46:59 +0100

    skill set

--- Portfolio/src/app/app.component.ts
+++ Portfolio/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { AboutMeComponent } from './components/about-me/about-me.component';
+import { SkillSetComponent } from './components/skill-set/skill-set.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, AboutMeComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, AboutMeComponent, SkillSetComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ Portfolio/src/app/components/skill-set/skill-set.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { CommonModule, NgFor } from '@angular/common';
+
+@Component({
+  selector: 'app-skill-set',
+  standalone: true,
+  imports: [CommonModule, NgFor],
+  templateUrl: './skill-set.component.html',
+  styleUrl: './skill-set.component.scss'
+})
+export class SkillSetComponent {
+  imageUrls: string[] = [
+    './../../../assets/img/skills/angular.png',
+    './../../../assets/img/skills/typescript.png',
+    './../../../assets/img/skills/javascript.png',
+    './../../../assets/img/skills/html.png',
+    './../../../assets/img/skills/scrum.png',
+    './../../../assets/img/skills/firebase.png',
+    './../../../assets/img/skills/git.png',
+    './../../../assets/img/skills/css.png',
+    './../../../assets/img/skills/api.png',
+    './../../../assets/img/skills/material_design.png',
+  ];
+
+  titles: string[] = [
+    'Angular', 'TypeScript', 'JavaScript', 'HTML', 'Scrum', 'Firebase', 'GIT', 'CSS', 'Rest-Api', 'Material design'
+  ]
+}
