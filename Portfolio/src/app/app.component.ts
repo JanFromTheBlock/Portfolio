4e064cc88cd4de60fe60f2dabc617d10f41d61d5
JanFromTheBlock <133579648+JanFromTheBlock@users.noreply.github.com>
2024-01-12 13:45:37 +0100

    my-work

--- Portfolio/src/app/app.component.ts
+++ Portfolio/src/app/app.component.ts
@@ -4,11 +4,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { AboutMeComponent } from './components/about-me/about-me.component';
 import { SkillSetComponent } from './components/skill-set/skill-set.component';
+import { MyWorkComponent } from './components/my-work/my-work.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, AboutMeComponent, SkillSetComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, AboutMeComponent, SkillSetComponent, MyWorkComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
