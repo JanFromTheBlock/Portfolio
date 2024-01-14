05506124ff23f1c943663a14ef17faef98ad191d
JanFromTheBlock <133579648+JanFromTheBlock@users.noreply.github.com>
2024-01-14 22:33:21 +0100

    contact form part1

--- Portfolio/src/app/components/my-work/my-work.component.ts
+++ Portfolio/src/app/components/my-work/my-work.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 @Component({
   selector: 'app-my-work',
   standalone: true,
-  imports: [],
+  imports: [ContactFormComponent],
   templateUrl: './my-work.component.html',
   styleUrl: './my-work.component.scss'
 })
