047a6c8c74c55af5ff1809dedf85ebc576eb4cf9
JanFromTheBlock <133579648+JanFromTheBlock@users.noreply.github.com>
2024-01-16 19:06:41 +0100

    footer

--- Portfolio/src/app/app.component.ts
+++ Portfolio/src/app/app.component.ts
@@ -5,11 +5,12 @@ import { HeaderComponent } from './components/header/header.component';
 import { AboutMeComponent } from './components/about-me/about-me.component';
 import { SkillSetComponent } from './components/skill-set/skill-set.component';
 import { MyWorkComponent } from './components/my-work/my-work.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HeaderComponent, AboutMeComponent, SkillSetComponent, MyWorkComponent],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, AboutMeComponent, SkillSetComponent, MyWorkComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
