a84bd7441f948c032509e4e1ab58710c09bf4e9f
JanFromTheBlock <133579648+JanFromTheBlock@users.noreply.github.com>
2024-01-25 22:11:54 +0100

    animation & menu-responsive part1

--- Portfolio/src/app/components/header/header.component.ts
+++ Portfolio/src/app/components/header/header.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from './navbar/navbar.component';
+import { BurgerMenuComponent } from './burger-menu/burger-menu.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, BurgerMenuComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
