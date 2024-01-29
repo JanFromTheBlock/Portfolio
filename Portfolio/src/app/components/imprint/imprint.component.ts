6eb99c2ec3e5da53fbb04fb92d7ae9fb05f600e2
JanFromTheBlock <133579648+JanFromTheBlock@users.noreply.github.com>
2024-01-29 15:23:11 +0100

    fix bugs

--- Portfolio/src/app/components/imprint/imprint.component.ts
+++ Portfolio/src/app/components/imprint/imprint.component.ts
@@ -1,14 +1,21 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from '../footer/footer.component';
 import { NavbarComponent } from '../header/navbar/navbar.component';
+import { BurgerMenuComponent } from '../header/burger-menu/burger-menu.component';
 
 @Component({
   selector: 'app-imprint',
   standalone: true,
-  imports: [FooterComponent, NavbarComponent],
+  imports: [FooterComponent, NavbarComponent, BurgerMenuComponent],
   templateUrl: './imprint.component.html',
   styleUrl: './imprint.component.scss'
 })
 export class ImprintComponent {
-
+  openMenu(){
+    document.getElementById('burger-menu')?.classList.remove('app-burger-menu-closed')
+  }
+  
+  doNotCloseMenu(event: any){
+    event.stopPropagation();
+  }
 }
--- Portfolio/src/app/components/privacy-policy/privacy-policy.component.ts
+++ Portfolio/src/app/components/privacy-policy/privacy-policy.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../header/navbar/navbar.component';
+import { BurgerMenuComponent } from '../header/burger-menu/burger-menu.component';
 
 @Component({
   selector: 'app-privacy-policy',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, BurgerMenuComponent],
   templateUrl: './privacy-policy.component.html',
   styleUrl: './privacy-policy.component.scss'
 })
 export class PrivacyPolicyComponent {
-
+  openMenu(){
+    document.getElementById('burger-menu')?.classList.remove('app-burger-menu-closed')
+  }
+  
+  doNotCloseMenu(event: any){
+    event.stopPropagation();
+  }
 }
