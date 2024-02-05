c787ce9e80f455a2a0b7cf10ed74fa05c2ca47ec
JanFromTheBlock <133579648+JanFromTheBlock@users.noreply.github.com>
2024-02-05 16:47:06 +0100

    filter my work

--- Portfolio/src/app/components/my-work/my-work.component.ts
+++ Portfolio/src/app/components/my-work/my-work.component.ts
@@ -9,5 +9,29 @@ import { ContactFormComponent } from './contact-form/contact-form.component';
   styleUrl: './my-work.component.scss'
 })
 export class MyWorkComponent {
+  categories = ['all', 'javascript', 'angular', 'firebase'];
+  all = ['el-pollo-loco', 'join'];
+  javascript = ['el-pollo-loco', 'join'];
+  angular = [];
+  firebase = [];
 
+
+  filterWork(array: any, title: any){
+    for (let index = 0; index < this.all.length; index++) {
+      const element = this.all[index];
+      document.getElementById(element)?.classList.add('d-none');
+    }
+    for (let index = 0; index < array.length; index++) {
+      const element = array[index];
+      document.getElementById(element)?.classList.remove('d-none');
+    };
+
+    for (let index = 0; index < this.categories.length; index++) {
+      const element = this.categories[index];
+      document.getElementById('button-' + element)?.classList.remove('active');
+    }
+
+    document.getElementById('button-'+ title)?.classList.add('active');
+
+  }
 }
